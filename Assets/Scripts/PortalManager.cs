c95d8f3610d90f20f92ee7cd9c9d31632cd8d057
Chen5454 <74614370+Chen5454@users.noreply.github.com>
2023-02-03 14:04:26 +0200

    Update

--- Assets/Scripts/PortalManager.cs
+++ Assets/Scripts/PortalManager.cs
@@ -1,21 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PortalManager : MonoBehaviour
 {
+    public DimensionsType gamestates;
+    private DimensionsType previousGamestate;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-   
+
         if (other.CompareTag("Root"))
         {
             Debug.Log("Root Detected");
 
-            DimensionsType[] dimensions = (DimensionsType[])System.Enum.GetValues(typeof(DimensionsType));
-            int randomIndex = UnityEngine.Random.Range(0, dimensions.Length);
-            DimensionsType randomDimension = dimensions[randomIndex];
-            DimensionLayersManager.Instance.ChangeDimension(randomDimension);
+            if (previousGamestate == 0)
+            {
+                // first time entering, store the current gamestate
+                previousGamestate = gamestates;
+            }
+            else
+            {
+                // change back to previous gamestate
+                gamestates = previousGamestate;
+                previousGamestate = 0;
+            }
+
+            DimensionLayersManager.Instance.ChangeDimension(gamestates);
         }
     }
 }
