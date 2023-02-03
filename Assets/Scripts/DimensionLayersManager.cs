3219f31ea1afcd7aa34f7dfe4b89ff338df25e6b
elor504 <74613768+elor504@users.noreply.github.com>
2023-02-03 11:36:19 +0200

    change dimension prototype

--- Assets/Scripts/DimensionLayersManager.cs
+++ Assets/Scripts/DimensionLayersManager.cs
@@ -4,19 +4,49 @@ using UnityEngine;
 
 public class DimensionLayersManager : MonoBehaviour
 {
+	private static DimensionLayersManager _instance;
+	public static DimensionLayersManager Instance => _instance;
+
 	[SerializeField] private List<DimensionLayer> _dimensionLayers = new List<DimensionLayer>();
 	private DimensionLayer _previousDimension;
 
+	[SerializeField] private int _backgroundLayerOrder = -100;
+	[SerializeField] private int _upperLayerOrder = -99;
+
+	private void Awake()
+	{
+		if (_instance == null)
+			_instance = this;
+		else if (_instance != this)
+			Destroy(this.gameObject);
+
+		_previousDimension = _dimensionLayers[0];
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.G))
+		{
+			ChangeDimension(DimensionsType.DIMENSION_2);
+		}
+	}
+
 	public void ChangeDimension(DimensionsType type)
 	{
+		if (GetDimension(type).GetDimensionType == _previousDimension.GetDimensionType)//attempting to change to the same dimension
+		{
+			Debug.LogError("Dimension Layer Manager: Attempting to change to the same dimension, canceling function");
+			return;
+		}
 		//vfx
 
 		//changing dimensions
+		GetDimension(type).SetLayerOrder(_upperLayerOrder);
 		GetDimension(type).SetDimensionActive(true);//get the wanted dimension and enable it
 
 		_previousDimension.SetDimensionActive(false);//disable the first dimension layer
+		_previousDimension.SetLayerOrder(_backgroundLayerOrder);
 		_previousDimension = GetDimension(type);//update the previous dimension to the new dimension
-
 
 	}
 	DimensionLayer GetDimension(DimensionsType type)
@@ -35,6 +65,10 @@ public class DimensionLayer
 	public void SetDimensionActive(bool isActive)
 	{
 		_layerGO.SetActive(isActive);
+	}
+	public void SetLayerOrder(int order)
+	{
+		_layerGO.GetComponent<SpriteRenderer>().sortingOrder = order;
 	}
 }
 
