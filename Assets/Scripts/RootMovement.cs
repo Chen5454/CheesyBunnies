0fa7ae27362f1fa83701c9df6d67a4ba53203808
elor504 <74613768+elor504@users.noreply.github.com>
2023-02-04 12:04:45 +0200

    roots rotation fix

--- Assets/Scripts/RootMovement.cs
+++ Assets/Scripts/RootMovement.cs
@@ -21,6 +21,7 @@ public class RootMovement : MonoBehaviour
     
 
 	[SerializeField] private Transform tipTrans;
+	[SerializeField] private LineRenderer tunnelLine;
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
@@ -48,7 +49,10 @@ public class RootMovement : MonoBehaviour
         lineRenderer.SetVertexCount(++numberOfPoints);
         lineRenderer.SetPosition(numberOfPoints - 1, transform.position);
 
-        Vector3 lastPosition = transform.position;
+		tunnelLine.SetVertexCount(numberOfPoints);
+		tunnelLine.SetPosition(numberOfPoints - 1, transform.position);
+
+		Vector3 lastPosition = transform.position;
         transform.position += direction * Time.deltaTime * speed;
         totalLength -= Vector3.Distance(lastPosition, transform.position);
 
